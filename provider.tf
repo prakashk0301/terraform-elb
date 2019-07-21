9ee6e453f2a5bfa5fd7129c2cd778a494a12f380
prakashk0301 <34745120+prakashk0301@users.noreply.github.com>
2019-07-21 09:59:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-central-1"
-  access_key = "AKIAVTTRNU2HQRH3ONOU"
-  secret_key = "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"
+  access_key = "***********************"
+  secret_key = "***********************"
 }
