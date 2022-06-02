16b9cc74f17a64c996dd2620fafd23277f49dd95
Prakash Kumar <34745120+prakashk0301@users.noreply.github.com>
2022-06-02 21:23:34 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,24 +1,24 @@
 variable "aws_vpc" {
     description = "VPC"
-    default = "vpc-6d8f7507"
+    default = "vpc-0e8467f076fa127e9"
 }
 
 variable "aws_subnet" {
     description = "aws subnet"
-    default = "subnet-29903243"
+    default = "subnet-0325154992ec9b4c5"
 }
 
 variable "aws_subnet2" {
     description = "aws subnet2"
-    default = "subnet-81b9a0fc"
+    default = "subnet-0fc8213f59cffd5fd"
 }
 
 variable "aws_key" {
     description = "aws key"
-    default = "DevOpsKey"
+    default = "aws-frankfurt"
 }
 
  variable "aws_ami" {
      description = "ami id"
-     default = "ami-076431be05aaf8080"
+     default = "ami-09439f09c55136ecf"
 }
